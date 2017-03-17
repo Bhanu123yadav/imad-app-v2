1a37536ecd78930c231308a4c14a1fdaa714c71f
Bhanu123yadav <bhanu.cds@gmail.com>
2017-03-17 19:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res){
     //articleName=article-One;
     //articles[articleName]={} content object for article one
-    var articleName=req.params.ArticleName;
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
