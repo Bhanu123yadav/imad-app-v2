40525fb735660d97b2070865f4e1203b621a191d
Bhanu123yadav <bhanu.cds@gmail.com>
2017-03-22 18:42:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ button.onclick=function(){
       }
       //Make the request
       request.open('GET','http://bhanu123yadav.imad.hasura-app.io/counter',true);
-      request.send(null );
+      request.send(null);
   };
 };
