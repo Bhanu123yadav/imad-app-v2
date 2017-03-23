d39a04f67bd8b174ce0aced5411cf3eb2af6449b
Bhanu123yadav <bhanu.cds@gmail.com>
2017-03-23 10:37:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,6 +44,8 @@ submit.onclick=function(){
          //Not done yet
   };
   //Make the request
+  var nameInput=document.getElementById("name");
+  var name=nameInput.value;
   request.open('GET','http://bhanu123yadav.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);   
 };
