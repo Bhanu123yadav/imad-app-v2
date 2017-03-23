5fc4c5f64c26afb8e6867d88464c9b096f5d2b08
Bhanu123yadav <bhanu.cds@gmail.com>
2017-03-23 09:59:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/counter', function(req,res){
 });
 
 var names=[];
-app.get('submit-name',function(req,res){//submit-name?name=xxx
+app.get('/submit-name',function(req,res){//submit-name?name=xxx
     //Get the name from the request
     var name=req.query.name;
     names.push(name);
