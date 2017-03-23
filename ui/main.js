e3383131e5471c8bb540920812557832f66ccefd
Bhanu123yadav <bhanu.cds@gmail.com>
2017-03-23 10:37:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,6 @@ button.onclick=function(){
   request.send(null);   
 };
 //Submit name
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
   //Create the request
